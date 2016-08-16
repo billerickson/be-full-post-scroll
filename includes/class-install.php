40f78cbb066ee0b129b5564dccb89199155b3fe7
Bill Erickson <bill.erickson@gmail.com>
2016-08-16 15:50:00 -0500

    separate method for plugin activation/deactivation

--- includes/class-install.php
+++ includes/class-install.php
@@ -20,17 +20,28 @@ class BE_Full_Post_Scroll_Install {
 	 */
 	public function __construct() {
 		
-		register_activation_hook( BE_FULL_POST_SCROLL_FILE, array( $this, 'flush' ) );
-		register_deactivation_hook( BE_FULL_POST_SCROLL_FILE, array( $this, 'flush' ) );
+		register_activation_hook( BE_FULL_POST_SCROLL_FILE, array( $this, 'activate' ) );
+		register_deactivation_hook( BE_FULL_POST_SCROLL_FILE, array( $this, 'deactivate' ) );
 
 	}
 	
 	/**
-	 * Flush rewrite rules
+	 * Activation
 	 *
 	 * @since 1.0.0
 	 */
-	function flush() {
+	function activate() {
+
+		be_full_post_scroll()->core->endpoint();
+		flush_rewrite_rules();
+	}
+	
+	/**
+	 * Deactivation
+	 *
+	 * @since 1.0.0
+	 */
+	function deactivate() {
 		
 		flush_rewrite_rules();
 	}
