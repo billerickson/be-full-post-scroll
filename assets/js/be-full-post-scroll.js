f0aff1d2309bc5d0b8722c5e5d50a8f07ab6eae4
Bill Erickson <bill.erickson@gmail.com>
2016-08-16 17:08:26 -0500

    only display console messages if debugging

--- assets/js/be-full-post-scroll.js
+++ assets/js/be-full-post-scroll.js
@@ -5,6 +5,7 @@ jQuery(function($){
 	var next = '.post-navigation a[rel="prev"]';
 	var offset = 2000;
 	var delay = 400;
+	var debug = false;
 
 	// internal variables
 	$(content_container).append( '<span class="load-more"></span>' );
@@ -19,8 +20,10 @@ jQuery(function($){
 	    delay: delay
 	};
 	
-	console.log( 'next: ' + next_url );
-	console.log( 'offset: ' + offset );
+	if( debug ) {
+		console.log( 'Next Post: ' + next_url );
+		console.log( 'Will load when offset = ' + offset );
+	}
 
 	$(window).scroll(function(){
 		if( ! loading && scrollHandling.allow && next_url ) {
@@ -28,22 +31,33 @@ jQuery(function($){
 			setTimeout(scrollHandling.reallow, scrollHandling.delay);
 			var current_offset = $(button).offset().top - $(window).scrollTop();
 			
-			console.log( 'current offset: ' + current_offset );
+			if( debug ) {
+				console.log( 'Current offset: ' + current_offset );
+			}
 			
 			if( offset > current_offset ) {
 				loading = true;
 				
-				console.log( 'loading next post' );
+				if( debug ) {
+					console.log( 'Loading next post' );
+				}
 				
 				$(next).remove();
 				$.get(next_url + '/partial/1', function(content) {
+
 					$(content_container).append( content );
 					$(content_container).append( button );
 					next_url = $(next).attr('href');
-					console.log( 'next: ' + next_url );
 					loading = false;
+					
+					if( debug ) {
+						console.log( 'Next post: ' + next_url );
+					}
+
 				}).fail(function(xhr, textStatus, e) {
-					console.log(xhr.responseText);
+					if( debug ) {
+						console.log(xhr.responseText);
+					}
 				});
 
 			}
