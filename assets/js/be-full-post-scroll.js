2c654cd9bda62170727e4ef445267f97a5aeafbe
Bill Erickson <bill.erickson@gmail.com>
2016-08-16 18:02:20 -0500

    change url on scroll

--- assets/js/be-full-post-scroll.js
+++ assets/js/be-full-post-scroll.js
@@ -2,6 +2,7 @@ jQuery(function($){
 
 	// @todo make these customizable through wp_localize_script()
 	var content_container = '.infinite-wrapper';
+	var post_container = '.infinite-scroll-post';
 	var next = '.post-navigation a[rel="prev"]';
 	var offset = 2000;
 	var delay = 400;
@@ -10,6 +11,7 @@ jQuery(function($){
 	// internal variables
 	$(content_container).append( '<span class="load-more"></span>' );
 	var button = $(content_container + ' .load-more');
+	$(post_container).attr('data-title', document.title).attr('data-url', window.location.href );
 	var next_url = $(next).attr('href');
 	var loading = false;
 	var scrollHandling = {
@@ -27,14 +29,33 @@ jQuery(function($){
 
 	$(window).scroll(function(){
 		if( ! loading && scrollHandling.allow && next_url ) {
+		
+			// Start the timer so we don't run this code too often
 			scrollHandling.allow = false;
 			setTimeout(scrollHandling.reallow, scrollHandling.delay);
-			var current_offset = $(button).offset().top - $(window).scrollTop();
 			
+			// Change URL if viewing a new post
+			var State = History.getState(); 
+			$( post_container ).each(function(){
+			    var top = window.pageYOffset;
+			    var distance = top - $(this).offset().top;
+			    var url = $(this).attr('data-url');
+			    var title = $(this).attr('data-title');
+			
+			    if (distance < 150 && distance > -150 && State.url != url) {
+			    	if( debug ) {
+			    		console.log( 'Changing URL' );
+			    	}
+			       History.pushState(null, null, url );
+					window.document.title = title;
+			    }
+			});
+			
+			// Load more posts if close enough to end of page
+			var current_offset = $(button).offset().top - $(window).scrollTop();
 			if( debug ) {
 				console.log( 'Current offset: ' + current_offset );
 			}
-			
 			if( offset > current_offset ) {
 				loading = true;
 				
@@ -44,7 +65,6 @@ jQuery(function($){
 				
 				$(next).remove();
 				$.get(next_url + '/partial/1', function(content) {
-
 					$(content_container).append( content );
 					$(content_container).append( button );
 					next_url = $(next).attr('href');
@@ -64,3 +84,16 @@ jQuery(function($){
 		}
 	});
 });
+
+function be_check_current_post() {
+	$('.hash').each(function () {
+	    var top = window.pageYOffset;
+	    var distance = top - $(this).offset().top;
+	    var hash = $(this).attr('href');
+	
+	    if (distance < 30 && distance > -30 && currentHash != hash) {
+	        alert(hash);
+	        currentHash = hash;
+	    }
+	});
+}
--- includes/class-core.php
+++ includes/class-core.php
@@ -36,8 +36,9 @@ class BE_Full_Post_Scroll_Core {
 		// Only enqueue on single posts
 		if( ! is_single() )
 			return;	
-			
-		wp_enqueue_script( 'be-full-post-scroll', BE_FULL_POST_SCROLL_URL . 'assets/js/be-full-post-scroll.js', array( 'jquery' ), BE_FULL_POST_SCROLL_VERSION, true );
+		
+		wp_enqueue_script( 'history', BE_FULL_POST_SCROLL_URL . 'assets/js/jquery.history.js', array( 'jquery' ), BE_FULL_POST_SCROLL_VERSION, true );	
+		wp_enqueue_script( 'be-full-post-scroll', BE_FULL_POST_SCROLL_URL . 'assets/js/be-full-post-scroll.js', array( 'jquery', 'history' ), BE_FULL_POST_SCROLL_VERSION, true );
 
 	}
 	
