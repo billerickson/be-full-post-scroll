f8cfa526586f97a7fad89a5631e6a247e7947488
Bill Erickson <bill.erickson@gmail.com>
2016-08-16 15:22:56 -0500

    initial commit

---
+++ be-full-post-scroll.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: BE Full Post Scroll
+ * Plugin URI:  https://github.com/billerickson/be-full-post-scroll
+ * GitHub URI: billerickson/be-full-post-scroll
+ * Description: When viewing a single post, dynamically load the next post as the user scrolls
+ * Author:      Bill Erickson
+ * Version:     1.0.0
+ *
+ * BE Full Post Scroll is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * BE Full Post Scroll is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EA Share Count. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    BE_Full_Post_Scroll
+ * @author     Bill Erickson
+ * @since      1.0.0
+ * @license    GPL-2.0+
+ * @copyright  Copyright (c) 2015
+ */
+ 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
